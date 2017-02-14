7b0f14b4301a5d2c6c6e104e56b275f1924120d9
姜宁桃 <jiangningtao@192.168.0.104>
2017-02-14 12:11:17 +0800

    Initial Commit

---
+++ touch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  touch
+//
+//  Created by 姜宁桃 on 2017/2/14.
+//  Copyright © 2017年 姜宁桃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ touch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  touch
+//
+//  Created by 姜宁桃 on 2017/2/14.
+//  Copyright © 2017年 姜宁桃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
